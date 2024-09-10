9b74b9e690cd17b3110ad7991281c42b04e4b5e3
orangetabillyn <162754494+nyelladev@users.noreply.github.com>
2024-09-09 20:30:39 -0700

    Initial Commit

---
+++ Metal-Engine/Metal_EngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metal_EngineApp.swift
+//  Metal-Engine
+//
+//  Created by Alleyn Murphy on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Metal_EngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
