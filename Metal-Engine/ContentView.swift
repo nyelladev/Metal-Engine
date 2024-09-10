6d5c54124640b8b77b0e756eea9713b5101f39c3
orangetabillyn <162754494+nyelladev@users.noreply.github.com>
2024-09-09 20:59:03 -0700

    Hello World Metal

--- Metal-Engine/ContentView.swift
+++ Metal-Engine/ContentView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            MetalView()
+                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width:2)
+            Text("Hello World")
         }
         .padding()
     }
